121cd144f0834aab398fb28cb9e007b2e972bf06
Mateus <mateusmenavila@gmail.com>
2024-11-23 14:31:28 -0300

    API v1.0 finalizada

--- server.js
+++ server.js
@@ -9,41 +9,47 @@ app.use(express.json())
 const intentions = []
 
 app.post('/intention', async (req, res) => {
+
     await prisma.intention.create({
         data: {
             origin_cep: req.body.origin_cep,
             destination_cep: req.body.destination_cep
         }
     })
+
     res.status(201).json(req.body)
 })
 
-app.get('/intention', (req, res) => {
-    res.status(200).json(intentions)
+app.put('/intention/:id', async (req, res) => {
+
+    await prisma.intention.update({
+        where: {
+            id: req.params.id
+        },
+        data: {
+            origin_cep: req.body.origin_cep,
+            destination_cep: req.body.destination_cep, 
+            lead_id: req.body.lead_id
+        }
+    })
+
+    res.status(201).json(req.body)
 })
 
-app.put('/intention/{intention_id}', (req, res) => {
+app.post('/lead', async (req, res) => {
+    await prisma.lead.create({
+        data: {
+            name: req.body.name,
+            email: req.body.email
+        }
+    })
 
-})
-
-app.post('/lead', (req, res) => {
-
+    res.status(201).json(req.body)
 })
 
 app.get('/docs', (req, res) => {
 
 })
 
-/*app.get('/usuarios', (req, res) => {
-    res.send('Ok, deu bom')
-})
-*/
-
-/* 
-
-mateusmenavila
-ili1kZiUDIUb9NH8
-
-*/
 
 app.listen(3000)
